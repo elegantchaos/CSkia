ef6e4f72739cc1c4ef5c97bebfae0934f61fc12f
Sam Deane <sam@elegantchaos.com>
2018-07-30 13:03:50 +0100

    added package

---
+++ Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CSkia"
+)
